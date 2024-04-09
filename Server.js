d9df6225a93270ed256100d734473d64bcb83b2a
Muhammad Hamza Siddiqui <m.hamza.siddiqui100@gmail.com>
2024-04-09 18:32:17 +0500

    products api

--- Server.js
+++ Server.js
@@ -2,18 +2,23 @@ const express = require("express");
 const routesProducts = require("./Routes/API/productsRoutes");
 const routesCategories = require("./Routes/API/categoriesRoutes");
 const routesSales = require("./Routes/API/salesRoutes");
+const cors = require("cors");
 const server = express();
-const Port = 6000;
+const Port = 4000;
 
 const Server = async () => {
+  const corsOptions = {
+    origin: "http://localhost:3000",
+  };
+
+  server.use(cors(corsOptions)); // Corrected from app.use to server.use
+
   try {
-    await server.use(express.json());
-    await server.use(routesProducts);
-    await server.use(routesCategories);
-    await server.use(routesSales);
-    await server.listen(Port, () =>
-      console.log(`Server started at Port ${Port}`)
-    );
+    server.use(express.json());
+    server.use(routesProducts);
+    server.use(routesCategories);
+    server.use(routesSales);
+    server.listen(Port, () => console.log(`Server started at Port ${Port}`));
   } catch (err) {
     console.log(err);
     process.exit(1);
