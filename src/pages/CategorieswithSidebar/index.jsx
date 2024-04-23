37cea86f879d48c0de6ac29025f3a26d2db5f407
Muhammad Hamza Siddiqui <m.hamza.siddiqui100@gmail.com>
2024-04-23 09:13:26 +0500

    updation product feature

--- src/pages/CategorieswithSidebar/index.jsx
+++ src/pages/CategorieswithSidebar/index.jsx
@@ -61,6 +61,13 @@ export default function CategorieswithSidebarPage() {
     modal.classList.toggle("flex");
   };
 
+  const handleUpdateToggle = () => {
+    setIsOpen(true);
+    const modal = document.getElementById("update-modal");
+    modal.classList.toggle("hidden");
+    modal.classList.toggle("flex");
+  };
+
   const handleDeleteToggle = () => {
     setIsOpen(true);
     const modal = document.getElementById("delete-modal");
@@ -71,6 +78,13 @@ export default function CategorieswithSidebarPage() {
   const handleModalHide = () => {
     setIsOpen(false);
     const modal = document.getElementById("authentication-modal");
+    modal.classList.add("hidden");
+    modal.classList.remove("flex");
+  };
+
+  const updateModalHide = () => {
+    setIsOpen(false);
+    const modal = document.getElementById("update-modal");
     modal.classList.add("hidden");
     modal.classList.remove("flex");
   };
@@ -94,12 +108,33 @@ export default function CategorieswithSidebarPage() {
       });
       console.log(response);
       if (!response.ok) {
-        throw new Error("Failed to add product");
+        if (response.status === 409)
+          throw new Error("Cannot add duplicate products");
+        else throw new Error("Failed to add product");
       }
       alert("Product added successfully!");
     } catch (error) {
-      console.error("Error adding product:", error);
-      alert("Failed to add product. Please try again later.");
+      alert(error.message);
+    }
+  };
+
+  const handleUpdationProduct = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:4000/updateproducts", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("Failed to update product");
+      }
+      alert("Product updated successfully!");
+    } catch (error) {
+      alert(error.message);
     }
   };
 
@@ -114,6 +149,9 @@ export default function CategorieswithSidebarPage() {
       });
       console.log(response);
       if (!response.ok) {
+        // console.log("HTTP error status:", response.status); // Log the HTTP status code
+        // console.log("Response body:", await response.text());
+
         throw new Error("Failed to delete product");
       }
       alert("Product deleted successfully");
@@ -343,6 +381,131 @@ export default function CategorieswithSidebarPage() {
         </div>
       </div>
 
+      {/* UPDATE PRODUCT MODAL */}
+      <div
+        id="update-modal"
+        tabIndex="-1"
+        aria-hidden="true"
+        className="hidden opacity-100  overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-white"
+      >
+        <div className="relative p-4 w-full max-w-md max-h-full">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+              <h3 className="text-xl  opacity-100 z-50  font-semibold text-gray-900 dark:text-white">
+                Update Product
+              </h3>
+              <button
+                type="button"
+                className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                data-modal-hide="update-modal"
+                onClick={updateModalHide}
+              >
+                <svg
+                  className="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                <span className="sr-only">Close modal</span>
+              </button>
+            </div>
+            <div className="p-4 md:p-5  opacity-100 z-50 background bg-white-A700">
+              <form
+                className="space-y-4"
+                action="#"
+                onSubmit={handleUpdationProduct}
+              >
+                <div>
+                  <label
+                    htmlFor="product_name"
+                    className="block mb-2 text-[1rem] font-semibold text-gray-900 dark:text-white opacity-100 z-50"
+                  >
+                    Product Name
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="product_name"
+                    onChange={(e) => handleChange(e, setFormData)}
+                    className="bg-gray-50 border border-gray-300 mt-4 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    placeholder="Enter product"
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="buying_price"
+                    className="block mb-2 text-[1rem] font-semibold text-gray-900 dark:text-white"
+                  >
+                    Buying Price
+                  </label>
+                  <input
+                    type="number"
+                    name="buying_price"
+                    id="buying_price"
+                    placeholder="Enter buying price"
+                    onChange={(e) => handleChange(e, setFormData)}
+                    className="bg-gray-50 border border-gray-300 mt-4 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    required
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="Quantity"
+                    className="block mb-2 text-[1rem] font-semibold text-gray-900 dark:text-white"
+                  >
+                    Quantity
+                  </label>
+                  <input
+                    type="number"
+                    name="quantity"
+                    id="quantity"
+                    placeholder="Enter quantity"
+                    onChange={(e) => handleChange(e, setFormData)}
+                    className="bg-gray-50 border border-gray-800 mt-4 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="Category_id"
+                    className="block mb-2 text-[1rem] font-semibold text-gray-900 dark:text-white"
+                  >
+                    Category ID
+                  </label>
+                  <input
+                    type="number"
+                    name="category_id"
+                    id="Category_id"
+                    placeholder="Enter category id"
+                    onChange={(e) => handleChange(e, setFormData)}
+                    className="bg-gray-50 border border-gray-300 mt-4 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    required
+                  />
+                </div>
+
+                <button
+                  type="submit"
+                  className="w-full text-white-A700 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="flex flex-col items-center justify-start w-full bg-white-A700">
         <Header className="flex flex-row justify-between items-center w-full p-6 sm:p-5 bg-white-A700" />
 
@@ -383,6 +546,21 @@ export default function CategorieswithSidebarPage() {
                       }}
                     >
                       Delete Product
+                    </button>
+                  </Text>
+
+                  {/* UPDATE PRODUCT BUTTON */}
+                  <Text as="p" className="!text-gray-800 text-lg">
+                    <button
+                      data-modal-target="update-modal"
+                      data-modal-toggle="update-modal"
+                      className="block text-white-A700 bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      type="button"
+                      onClick={(e) => {
+                        handleUpdateToggle();
+                      }}
+                    >
+                      Update Product
                     </button>
                   </Text>
                 </div>
