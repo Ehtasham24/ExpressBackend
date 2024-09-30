887d89fcb392b76010feac20bff2cc8a8a2a86f9
ehtashamazhar <muhammad.ehtasham.azhar@gmail.com>
2024-09-30 15:36:01 +0500

    Removed duplicates and kept only the files from the Main folder

--- Main/Routes/API/categoriesRoutes.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const routes=express.Router();
-const {getCategories}=require('../../Controller/categoriesController')
-
-routes.get('/categories',getCategories);
-
-module.exports=routes
